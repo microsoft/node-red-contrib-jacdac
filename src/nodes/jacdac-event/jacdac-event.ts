03eacde9aac9b96cd2a2235aae6a02b2bea6bdf4
Peli de Halleux <pelikhan@users.noreply.github.com>
2021-08-01 21:32:34 +0200

    feat: node-red 2.0 (#5)
    * updating to node red 2.0
    
    * updated lock file
    
    * prettier settings
    
    * feat: 2.0

--- src/nodes/jacdac-event/jacdac-event.ts
+++ src/nodes/jacdac-event/jacdac-event.ts
@@ -1,57 +1,57 @@
-import { NodeInitializer } from "node-red";
-import { JacdacEventNode, JacdacEventNodeDef } from "./modules/types";
-import { cleanPayload, connectNode } from "../shared/bus";
-import { EVENT, JDDevice, JDEvent } from "jacdac-ts";
+import { NodeInitializer } from "node-red"
+import { JacdacEventNode, JacdacEventNodeDef } from "./modules/types"
+import { cleanPayload, connectNode } from "../shared/bus"
+import { EVENT, JDDevice, JDEvent } from "jacdac-ts"
 import {
-  createDeviceFilter,
-  createEventFilter,
-  createServiceFilter,
-} from "../shared/filters";
+    createDeviceFilter,
+    createEventFilter,
+    createServiceFilter,
+} from "../shared/filters"
 
 const nodeInit: NodeInitializer = (RED): void => {
-  function JacdacEventNodeConstructor(
-    this: JacdacEventNode,
-    config: JacdacEventNodeDef
-  ): void {
-    RED.nodes.createNode(this, config);
+    function JacdacEventNodeConstructor(
+        this: JacdacEventNode,
+        config: JacdacEventNodeDef
+    ): void {
+        RED.nodes.createNode(this, config)
 
-    const filterDevice = createDeviceFilter(config);
-    const filterService = createServiceFilter(config);
-    const filterEvent = createEventFilter(config);
+        const filterDevice = createDeviceFilter(config)
+        const filterService = createServiceFilter(config)
+        const filterEvent = createEventFilter(config)
 
-    const sendEvent = (evt: JDEvent) => {
-      const { service } = evt;
-      const { device } = service;
-      this.debug(`event ${evt}`);
-      this.send({
-        payload: cleanPayload({
-          deviceId: device.deviceId,
-          deviceShortId: device.shortId,
-          serviceIndex: service.serviceIndex,
-          serviceInstanceName: service.instanceName,
-          serviceName: service.name,
-          eventCode: evt.code,
-          eventName: evt.name,
-          data: evt.data,
-        }),
-      });
-    };
-
-    const registerDevice = (dev: JDDevice) => {
-      if (!filterDevice(dev)) return;
-
-      this.log(`registering device ${dev}`);
-      for (const srv of dev.services().filter(filterService)) {
-        for (const evt of srv.events.filter(filterEvent)) {
-          this.log(`registering event ${evt}`);
-          evt.on(EVENT, sendEvent);
+        const sendEvent = (evt: JDEvent) => {
+            const { service } = evt
+            const { device } = service
+            this.debug(`event ${evt}`)
+            this.send({
+                payload: cleanPayload({
+                    deviceId: device.deviceId,
+                    deviceShortId: device.shortId,
+                    serviceIndex: service.serviceIndex,
+                    serviceInstanceName: service.instanceName,
+                    serviceName: service.name,
+                    eventCode: evt.code,
+                    eventName: evt.name,
+                    data: evt.data,
+                }),
+            })
         }
-      }
-    };
-    connectNode(this, registerDevice);
-  }
 
-  RED.nodes.registerType("jacdac-event", JacdacEventNodeConstructor);
-};
+        const registerDevice = (dev: JDDevice) => {
+            if (!filterDevice(dev)) return
 
-export = nodeInit;
+            this.log(`registering device ${dev}`)
+            for (const srv of dev.services().filter(filterService)) {
+                for (const evt of srv.events.filter(filterEvent)) {
+                    this.log(`registering event ${evt}`)
+                    evt.on(EVENT, sendEvent)
+                }
+            }
+        }
+        connectNode(this, registerDevice)
+    }
+
+    RED.nodes.registerType("jacdac-event", JacdacEventNodeConstructor)
+}
+
+export = nodeInit
