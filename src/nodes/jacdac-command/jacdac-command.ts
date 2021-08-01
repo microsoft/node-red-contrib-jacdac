b7d6bc0302499e9dbf7e984994e1a462bf14f17a
pelikhan <jhalleux@microsoft.com>
2021-08-01 13:19:30 +0200

    prettier settings

--- src/nodes/jacdac-command/jacdac-command.ts
+++ src/nodes/jacdac-command/jacdac-command.ts
@@ -1,48 +1,48 @@
-import { jdpack, objectToUnpacked } from "jacdac-ts";
-import { NodeInitializer } from "node-red";
-import { bus, connectStatus } from "../shared/bus";
+import { jdpack, objectToUnpacked } from "jacdac-ts"
+import { NodeInitializer } from "node-red"
+import { bus, connectStatus } from "../shared/bus"
 import {
-  createCommandFilter,
-  createDeviceFilter,
-  createServiceFilter,
-} from "../shared/filters";
-import { JacdacCommandNode, JacdacCommandNodeDef } from "./modules/types";
+    createCommandFilter,
+    createDeviceFilter,
+    createServiceFilter,
+} from "../shared/filters"
+import { JacdacCommandNode, JacdacCommandNodeDef } from "./modules/types"
 
 const nodeInit: NodeInitializer = (RED): void => {
-  function JacdacCommandNodeConstructor(
-    this: JacdacCommandNode,
-    config: JacdacCommandNodeDef
-  ): void {
-    RED.nodes.createNode(this, config);
+    function JacdacCommandNodeConstructor(
+        this: JacdacCommandNode,
+        config: JacdacCommandNodeDef
+    ): void {
+        RED.nodes.createNode(this, config)
 
-    const filterDevice = createDeviceFilter(config);
-    const filterService = createServiceFilter(config);
-    const filterCommand = createCommandFilter(config);
+        const filterDevice = createDeviceFilter(config)
+        const filterService = createServiceFilter(config)
+        const filterCommand = createCommandFilter(config)
 
-    this.on("input", async (msg, send, done) => {
-      const { payload } = msg;
+        this.on("input", async (msg, send, done) => {
+            const { payload } = msg
 
-      for (const dev of bus
-        .devices({ ignoreSelf: true, announced: true })
-        .filter(filterDevice)) {
-        for (const srv of dev
-          .services({ specification: true })
-          .filter(filterService)) {
-          const cmd = filterCommand(srv);
-          if (cmd?.packFormat) {
-            const unpacked = objectToUnpacked(cmd, payload);
-            const data = jdpack(cmd.packFormat, unpacked);
-            await srv.sendCmdAsync(cmd.identifier, data);
-          }
-        }
-      }
-      done();
-    });
+            for (const dev of bus
+                .devices({ ignoreSelf: true, announced: true })
+                .filter(filterDevice)) {
+                for (const srv of dev
+                    .services({ specification: true })
+                    .filter(filterService)) {
+                    const cmd = filterCommand(srv)
+                    if (cmd?.packFormat) {
+                        const unpacked = objectToUnpacked(cmd, payload)
+                        const data = jdpack(cmd.packFormat, unpacked)
+                        await srv.sendCmdAsync(cmd.identifier, data)
+                    }
+                }
+            }
+            done()
+        })
 
-    connectStatus(this);
-  }
+        connectStatus(this)
+    }
 
-  RED.nodes.registerType("jacdac-command", JacdacCommandNodeConstructor);
-};
+    RED.nodes.registerType("jacdac-command", JacdacCommandNodeConstructor)
+}
 
-export = nodeInit;
+export = nodeInit
--- src/nodes/jacdac-report/jacdac-report.ts
+++ src/nodes/jacdac-report/jacdac-report.ts
@@ -1,66 +1,66 @@
-import { JDDevice, JDRegister, REPORT_RECEIVE, REPORT_UPDATE } from "jacdac-ts";
-import { NodeInitializer } from "node-red";
-import { cleanPayload, connectNode } from "../shared/bus";
+import { JDDevice, JDRegister, REPORT_RECEIVE, REPORT_UPDATE } from "jacdac-ts"
+import { NodeInitializer } from "node-red"
+import { cleanPayload, connectNode } from "../shared/bus"
 import {
-  createDeviceFilter,
-  createRegisterFilter,
-  createServiceFilter,
-} from "../shared/filters";
-import { JacdacReportNode, JacdacReportNodeDef } from "./modules/types";
+    createDeviceFilter,
+    createRegisterFilter,
+    createServiceFilter,
+} from "../shared/filters"
+import { JacdacReportNode, JacdacReportNodeDef } from "./modules/types"
 
 const nodeInit: NodeInitializer = (RED): void => {
-  function JacdacReportNodeConstructor(
-    this: JacdacReportNode,
-    config: JacdacReportNodeDef
-  ): void {
-    RED.nodes.createNode(this, config);
+    function JacdacReportNodeConstructor(
+        this: JacdacReportNode,
+        config: JacdacReportNodeDef
+    ): void {
+        RED.nodes.createNode(this, config)
 
-    const filterDevice = createDeviceFilter(config);
-    const filterService = createServiceFilter(config);
-    const filterRegister = createRegisterFilter(config);
+        const filterDevice = createDeviceFilter(config)
+        const filterService = createServiceFilter(config)
+        const filterRegister = createRegisterFilter(config)
 
-    const sendRegister = (reg: JDRegister) => {
-      const { service: srv } = reg;
-      const { device: dev } = srv;
-      this.send({
-        payload: cleanPayload({
-          data: reg.objectValue,
-          deviceShortId: dev.shortId,
-          serviceIndex: srv.serviceIndex,
-          serviceName: srv.name,
-          registerName: reg.name,
+        const sendRegister = (reg: JDRegister) => {
+            const { service: srv } = reg
+            const { device: dev } = srv
+            this.send({
+                payload: cleanPayload({
+                    data: reg.objectValue,
+                    deviceShortId: dev.shortId,
+                    serviceIndex: srv.serviceIndex,
+                    serviceName: srv.name,
+                    registerName: reg.name,
 
-          // low-level info
-          deviceId: dev.deviceId,
-          serviceClass: srv.serviceClass,
-          registerCode: reg.code,
-        }),
-      });
-    };
-
-    const { updates } = config;
-    const reportEvent = updates ? REPORT_UPDATE : REPORT_RECEIVE;
-
-    const registerDevice = (dev: JDDevice) => {
-      this.log(`registering device ${dev}`);
-      if (filterDevice(dev)) {
-        for (const srv of dev.services().filter(filterService)) {
-          this.log(`scanning service ${srv.name}`);
-          for (const reg of srv.registers().filter(filterRegister)) {
-            this.log(`registering register ${reg.name}`);
-            // register this register will automatically
-            // automatically have the bus
-            // refresh its value
-            reg.on(reportEvent, sendRegister);
-          }
+                    // low-level info
+                    deviceId: dev.deviceId,
+                    serviceClass: srv.serviceClass,
+                    registerCode: reg.code,
+                }),
+            })
         }
-      }
-    };
 
-    connectNode(this, registerDevice);
-  }
+        const { updates } = config
+        const reportEvent = updates ? REPORT_UPDATE : REPORT_RECEIVE
 
-  RED.nodes.registerType("jacdac-report", JacdacReportNodeConstructor);
-};
+        const registerDevice = (dev: JDDevice) => {
+            this.log(`registering device ${dev}`)
+            if (filterDevice(dev)) {
+                for (const srv of dev.services().filter(filterService)) {
+                    this.log(`scanning service ${srv.name}`)
+                    for (const reg of srv.registers().filter(filterRegister)) {
+                        this.log(`registering register ${reg.name}`)
+                        // register this register will automatically
+                        // automatically have the bus
+                        // refresh its value
+                        reg.on(reportEvent, sendRegister)
+                    }
+                }
+            }
+        }
 
-export = nodeInit;
+        connectNode(this, registerDevice)
+    }
+
+    RED.nodes.registerType("jacdac-report", JacdacReportNodeConstructor)
+}
+
+export = nodeInit
