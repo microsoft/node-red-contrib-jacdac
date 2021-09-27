5d7aaccce689cdda7020fe5996bca397f399a8a0
pelikhan <jhalleux@microsoft.com>
2021-09-27 15:04:53 -0700

    support for iframebridge

--- src/nodes/shared/bus.ts
+++ src/nodes/shared/bus.ts
@@ -1,5 +1,13 @@
-import { Node } from "node-red";
-import { createUSBTransport, createNodeUSBOptions, JDBus, CONNECTION_STATE, JDDevice, DEVICE_ANNOUNCE, CONNECT } from "jacdac-ts"
+import { Node } from "node-red"
+import {
+    createUSBTransport,
+    createNodeUSBOptions,
+    JDBus,
+    CONNECTION_STATE,
+    JDDevice,
+    DEVICE_ANNOUNCE,
+    CONNECT,
+} from "jacdac-ts"
 
 const opts = createNodeUSBOptions()
 const transport = createUSBTransport(opts)
@@ -8,8 +16,11 @@ export const bus = new JDBus([transport])
 export function connectStatus(node: Node) {
     const updateStatus = () => {
         node.log(`connection: ${bus.connected ? "connected" : "disconnected"}`)
-        node.status(bus.connected ? { fill: "green", shape: "dot", text: "connected" }
-            : { fill: "red", shape: "ring" })
+        node.status(
+            bus.connected
+                ? { fill: "green", shape: "dot", text: "connected" }
+                : { fill: "red", shape: "ring" }
+        )
     }
 
     bus.on(CONNECTION_STATE, updateStatus)
@@ -18,9 +29,12 @@ export function connectStatus(node: Node) {
     bus.connect()
 }
 
-export function connectNode(node: Node, registerDevice: (dev: JDDevice) => void) {
+export function connectNode(
+    node: Node,
+    registerDevice: (dev: JDDevice) => void
+) {
     const registerAllDevices = () => {
-        for (const dev of bus.devices({ ignoreSelf: true }))
+        for (const dev of bus.devices({ ignoreInfrastructure: true }))
             registerDevice(dev)
     }
 
@@ -32,9 +46,8 @@ export function connectNode(node: Node, registerDevice: (dev: JDDevice) => void)
 }
 
 export function cleanPayload(payload: any) {
-    for(const key in payload) {
-        if (payload[key] === undefined)
-            delete payload[key]
+    for (const key in payload) {
+        if (payload[key] === undefined) delete payload[key]
     }
     return payload
 }
